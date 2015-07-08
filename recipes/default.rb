7195561a57b867cbe28efd75c041ee63966ca484
unknown <ma892429@U002364.bmrn.com>
2015-07-08 07:06:41 -0700

    add support for ubuntu 10.04

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,12 @@
-package "git"
 
-log "well, that was too easy"
+if node['platform'] == "ubuntu" && node['platform_version'].to_f <= 10.04
+  package "git-core"
+  else
+    package "git"
+  end
+
+log "Well, that was too easy"
+
+
+
+
